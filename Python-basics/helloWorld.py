f935629029840498c8b71ceca473b670063b42c3
Taptaplit <vismayp301@gmail.com>
2021-01-30 17:29:38 -0500

    first commit

---
+++ Python-basics/helloWorld.py
@@ -0,0 +1,16 @@
+number = 90
+string = 'hello world'
+float_num = 90.39
+
+print(f'''
+      Number: {number}
+      Float: {float_num}
+      String: {string}
+      
+      ''')
+
+
+def basic():
+    print('BASIC FUNCTION')
+    
+basic()
