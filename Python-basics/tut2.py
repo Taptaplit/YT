0d0fb2fdf088f430f0da282fd9699e0c1b91fda7
Taptaplit <vismayp301@gmail.com>
2021-01-31 13:35:35 -0500

    tut-2

---
+++ Python-basics/tut2.py
@@ -0,0 +1,22 @@
+
+#challenge
+# title = 'python basics #2'
+# description = 'python basics #2 video'
+# find if python is in title and description
+# also check if basics in in description and title
+
+description = 'hoi'
+number = 90
+
+if description == 'hoi':
+    print('description == hoi')
+elif description != 'hoi':
+    print(description + '!= hoi')
+    
+    
+if 'h' in description:
+    print('h')
+    
+    
+if number > 80:
+    print('number > 80')
